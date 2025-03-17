05c0346bbb636290ff091d8d0214362041a56a6f
Halil Arda Ozongun <halilardaozongun@gmail.com>
2025-03-17 21:20:53 +0300

    Created initals of project, added package.json and others

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json()); // Middleware to handle JSON requests
+
+app.get("/", (req, res) => {
+  res.send("sude irem elif yunus arda");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
